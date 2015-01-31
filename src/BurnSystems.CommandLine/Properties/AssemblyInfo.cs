cbd668d040d76322b2e0ccece495fd171ab61491
Martin Brenn <brenn@depon.net>
2015-01-30 22:51:00 -0500

    NuSpec adde

--- src/BurnSystems.CommandLine/Properties/AssemblyInfo.cs
+++ src/BurnSystems.CommandLine/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BurnSystems.CommandLine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The command line parser prepeares the arguments to named and unnamed arguments")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Martin Brenn")]
 [assembly: AssemblyProduct("BurnSystems.CommandLine")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
